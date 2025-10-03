f6a608a63bc210b02d1f288f6bc4bfeb6735b30c
Torchina Alena <helentrufanov@gmail.com>
2025-10-03 23:55:01 +0400

    Create server.py

---
+++ app/server.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
+class Config(object):
+    SQLALCHEMY_DATABASE_URI = 'postgresql://user:password@localhost/dbname'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+db = SQLAlchemy()
+
+
+def create_app():
+    server = Flask(__name__)
+    server.config.from_object(Config)
+    db.init_app(server)
+
+    from .dashboard import create_dashapp
+    dash_app = create_dashapp(server)
+    return server 
