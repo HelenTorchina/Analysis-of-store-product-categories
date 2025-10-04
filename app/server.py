2773b52da37376921aecaad0c0196bbd548ff300
Torchina Alena <helentrufanov@gmail.com>
2025-10-04 14:45:27 +0400

    Update server.py

--- app/server.py
+++ app/server.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-
+# Подключение к базе данных
 class Config(object):
     SQLALCHEMY_DATABASE_URI = 'postgresql://user:password@localhost/dbname'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
@@ -9,7 +9,7 @@ class Config(object):
 
 db = SQLAlchemy()
 
-
+# Создание приложения
 def create_app():
     server = Flask(__name__)
     server.config.from_object(Config)
